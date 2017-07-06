cd13df449c35e292fdec56ce5870d4ab4622480d
SHANIL FERNANDO <shanilfernando@hotmail.com>
2017-07-06 18:53:53 +0400

    Initial commit

---
+++ docs/main-aot.js
@@ -0,0 +1,4 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModuleNgFactory } from '../out-tsc/src/app/app.module.ngfactory';
+platformBrowserDynamic().bootstrapModuleFactory(AppModuleNgFactory);
+//# sourceMappingURL=main-aot.js.map
